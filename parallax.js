6ae925b0a6bd2de06aad43f5ce6abce13203b94f
Alexandra <banica@9221@hotmail.be>
2021-05-04 15:13:19 +0200

    moving animation partly done

--- parallax.js
+++ parallax.js
@@ -1,15 +1,45 @@
-function animation(){
-    let ground = document.getElementById('ground');
-    let startingPosition = 0;
-    clearInterval(id);
-    id = setInterval(animate,2000);
+const character = document.querySelector('.character');
+const pixelSubstraction = 10;
 
-    function animate(){
-        if (startingPosition === 300){
-            clearInterval(id);
-        }
-        else {
-            startingPosition++;
-        }
-    }
+character.style.position = 'relative';
+character.style.left = '350px';
+character.style.top = '385px';
+
+function keyControls(e) {
+    let keys = e.key
+   switch(keys){
+       case 'ArrowLeft':
+           moveLeft();
+           break;
+       case 'ArrowRight':
+           moveRight();
+           break;
+       case 'ArrowUp':
+           jump();
+           break;
+   }
+   console.log(keys)
 }
+
+function moveLeft() {
+    character.style.left = parseInt(character.style.left) - pixelSubstraction + 'px';
+}
+
+function moveRight() {
+    character.style.left = parseInt(character.style.left) + pixelSubstraction + 'px';
+}
+
+function jump() {
+    character.style.top = parseInt(character.style.left) + pixelSubstraction + 'px';
+}
+
+
+document.addEventListener('keydown', function (e) {
+    keyControls(e);
+});
+
+
+
+
+
+
